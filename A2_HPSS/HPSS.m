dedabc969946cf71272d710db5b703940895acc6
Finomnis <martin.h.stumpf@gmail.com>
2015-11-09 16:38:56 +0100

    added sources for A2_HPSS

---
+++ A2_HPSS/HPSS.m
@@ -0,0 +1,20 @@
+function [x_h,x_p] = HPSS(x,N,H,w,fs,lh_sec,lp_Hz)
+% x:      Input signal
+% N:      Frame length
+% H:      Hopsize
+% w:      Window function of length N
+% fs:     Sampling rate of x
+% lh_sec: Horizontal median filter length given in seconds
+% lp_Hz:  Percussive median filter length given in Hertz
+
+%% stft
+
+%% power spectrogram
+
+%% median filtering
+
+%% masking
+
+%% istft
+
+end
---
+++ A2_HPSS/onsetDetection.m
@@ -0,0 +1,13 @@
+function novelty = onsetDetection(x,N,H,J)
+% x:      Input signal
+% N:      Frame length
+% H:      Hopsize
+% J:      Neighborhood
+
+%% short-time power
+
+%% local average
+
+%% novelty
+
+end
