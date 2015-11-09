b34712fe36094fbb9f6f778d964936a19a834633
Finomnis <martin.h.stumpf@gmail.com>
2015-11-09 17:26:37 +0100

    added lab_experiment 1

--- A2_HPSS/LabExperiment_1to4.m
+++ A2_HPSS/LabExperiment_1to4.m
@@ -4,6 +4,24 @@
 
 addpath('providedCode');
 
+[x,fs]=audioread('Data/CastanetsViolin.wav');
+
+x_t = stft(x, 1024, 512, win('sin', 1024));
+visualize_matrix(x_t, 10);
+
+% window narrower
+% => better time resolution
+%   => better representation of the percussive(vertical) part,
+%      as percussive signals are time-precise over a large frequency band
+% => worse frequency resolution
+%   => harmonic part gets blurred, as harmonic signals are
+%      frequency-precise over a large time frame
+x_t = stft(x, 128, 64, win('sin', 128));
+visualize_matrix(x_t, 10);
+
+% opposite of everything written above
+x_t = stft(x, 8192, 4096, win('sin', 8192));
+visualize_matrix(x_t, 10);
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % LAB EXPERIMENT 2
