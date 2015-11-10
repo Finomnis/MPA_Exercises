5b94c37ecf92832acedcb5edc5a4d892efdf4f02
Finomnis <martin.h.stumpf@gmail.com>
2015-11-10 15:00:14 +0100

    added A5

--- A2_HPSS/HPSS.m
+++ A2_HPSS/HPSS.m
@@ -8,13 +8,30 @@ function [x_h,x_p] = HPSS(x,N,H,w,fs,lh_sec,lp_Hz)
 % lp_Hz:  Percussive median filter length given in Hertz
 
 %% stft
+x_t = stft(x, N, H, w);
 
 %% power spectrogram
+x_p = abs(x_t);
 
 %% median filtering
+l_h = ceil((fs./H).*lh_sec);
+l_p = ceil((N./fs).*lp_Hz);
+x_p_h = medianFilter(x_p, l_h, 'h');
+x_p_p = medianFilter(x_p, l_p, 'p');
 
 %% masking
+m_h = x_p_h > x_p_p;
+m_p = x_p_p >= x_p_h;
+
+visualize_matrix(m_h);
+visualize_matrix(m_p);
+x_t_h = zeros(size(x_t));
+x_t_p = zeros(size(x_t));
+x_t_h(m_h) = x_t(m_h);
+x_t_p(m_p) = x_t(m_p);
 
 %% istft
+x_h = istft(x_t_h, H, w);
+x_p = istft(x_t_p, H, w);
 
 end
--- A2_HPSS/LabExperiment_5.m
+++ A2_HPSS/LabExperiment_5.m
@@ -3,3 +3,19 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 addpath('providedCode');
+
+[x,fs] = audioread('Data/DrumSolo.wav');
+[x_h, x_p] = HPSS(x,1024, 512, win('sin',1024), fs, 0.2, 500);
+audiowrite('Data_out/DrumSolo_h.wav', x_h, fs);
+audiowrite('Data_out/DrumSolo_p.wav', x_p, fs);
+
+[x,fs] = audioread('Data/Stepdad.wav');
+[x_h, x_p] = HPSS(x,1024, 512, win('sin',1024), fs, 0.2, 500);
+audiowrite('Data_out/Stepdad_h.wav', x_h, fs);
+audiowrite('Data_out/Stepdad_p.wav', x_p, fs);
+
+[x,fs] = audioread('Data/Applause.wav');
+[x_h, x_p] = HPSS(x,1024, 512, win('sin',1024), fs, 0.2, 500);
+audiowrite('Data_out/Applause_h.wav', x_h, fs);
+audiowrite('Data_out/Applause_p.wav', x_p, fs);
+
