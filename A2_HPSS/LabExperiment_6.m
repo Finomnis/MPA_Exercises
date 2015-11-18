0739882aaf6a1ca431c56999bfea51f11652d12d
Finomnis <martin.h.stumpf@gmail.com>
2015-11-18 15:24:33 -0600

    added a6

--- A2_HPSS/LabExperiment_6.m
+++ A2_HPSS/LabExperiment_6.m
@@ -3,3 +3,12 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 addpath('providedCode');
+[x,fs]=audioread('Data/CastanetsViolin.wav');
+
+x_chroma = simple_chroma(x, 4410, 2205, fs);
+visualize_simpleChroma(x_chroma, 2205, fs);
+
+[x_h, x_p] = HPSS(x,1024, 512, win('sin',1024), fs, 0.2, 500);
+
+x_h_chroma = simple_chroma(x_h, 4410, 2205, fs);
+visualize_simpleChroma(x_h_chroma, 2205, fs);
